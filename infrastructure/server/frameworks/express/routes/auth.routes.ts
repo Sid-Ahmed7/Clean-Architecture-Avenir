cd9f36d83d29a66bd0e32aae4574dc227a1e010c
LuzBoger <arthurbrouard2003@gmail.com>
2025-12-02 10:49:41 +0100

    modification des routes / controller plus ajout d'un header specifique pour l'administrateur

--- infrastructure/server/frameworks/express/routes/auth.routes.ts
+++ infrastructure/server/frameworks/express/routes/auth.routes.ts
@@ -24,9 +24,11 @@ router.post("/register", (req, res) => authController.register(req,res));
 router.get("/confirm", (req, res) => authController.confirmRegistration(req, res));
 router.post("/login", (req, res) => authController.login(req,res));
 router.post("/refresh-token", (req, res) => authController.refreshToken(req,res));
-router.get("/profile", verifyTokenAccess, authorizeRoles([RoleEnum.CLIENT, RoleEnum.BANK_ADVISOR]), (req, res) => authController.getUserProfile(req,res));
-router.post("/logout", verifyTokenAccess, authorizeRoles([RoleEnum.CLIENT, RoleEnum.BANK_MANAGER]), (req, res) => authController.logout(req,res));
+router.get("/profile", verifyTokenAccess, authorizeRoles([RoleEnum.CLIENT, RoleEnum.BANK_ADVISOR, RoleEnum.ADMIN, RoleEnum.BANK_MANAGER]), (req, res) => authController.getUserProfile(req,res));
+router.post("/logout", verifyTokenAccess, authorizeRoles([RoleEnum.CLIENT, RoleEnum.BANK_MANAGER, RoleEnum.ADMIN, RoleEnum.BANK_ADVISOR]), (req, res) => authController.logout(req,res));
 router.post("/register/advisor", (req, res) => authController.registerAdvisor(req,res));
 router.get("/getAdvisors", verifyTokenAccess, authorizeRoles([RoleEnum.BANK_ADVISOR]), (req, res) => authController.getAdvisors(req, res))
-
+router.post("/create-advisor", verifyTokenAccess, authorizeRoles([RoleEnum.BANK_MANAGER]), (req, res) => authController.registerAdvisor(req, res))
+router.post("/create-manager", verifyTokenAccess, authorizeRoles([RoleEnum.ADMIN]), (req, res) => authController.registerManager(req, res))
+router.post("/create-admin", (req, res) => authController.createAdmin(req, res));
 export default router;
