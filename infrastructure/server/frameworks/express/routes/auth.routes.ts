8eb810728f47f3ba4ebaa719b56fa65f39683ab2
Sofiane CHADILI <sofiane.chadili@hotmail.com>
2025-12-14 16:50:06 +0100

    maj

--- infrastructure/server/frameworks/express/routes/auth.routes.ts
+++ infrastructure/server/frameworks/express/routes/auth.routes.ts
@@ -19,7 +19,12 @@ router.post("/refresh-token", (req, res) => authController.refreshToken(req,res)
 router.get("/profile", verifyTokenAccess, authorizeRoles([RoleEnum.CLIENT, RoleEnum.BANK_ADVISOR, RoleEnum.BANK_MANAGER]), (req, res) => authController.getUserProfile(req,res));
 router.post("/logout", verifyTokenAccess, authorizeRoles([RoleEnum.CLIENT, RoleEnum.BANK_MANAGER, RoleEnum.BANK_ADVISOR]), (req, res) => authController.logout(req,res));
 router.post("/register/advisor", (req, res) => authController.registerAdvisor(req,res));
-router.get("/getAdvisors", verifyTokenAccess, authorizeRoles([RoleEnum.CLIENT, RoleEnum.BANK_ADVISOR, RoleEnum.BANK_MANAGER, RoleEnum.ADMIN]), (req, res) => authController.getAdvisors(req, res))
+router.get(
+  "/getAdvisors",
+  verifyTokenAccess,
+  authorizeRoles([RoleEnum.CLIENT, RoleEnum.BANK_ADVISOR, RoleEnum.BANK_MANAGER]),
+  (req, res) => authController.getAdvisors(req, res),
+);
 router.post("/create-advisor", verifyTokenAccess, authorizeRoles([RoleEnum.BANK_MANAGER]), (req, res) => authController.registerAdvisor(req, res))
 router.post("/create-manager", (req, res) => authController.registerManager(req, res))
 export default router;
