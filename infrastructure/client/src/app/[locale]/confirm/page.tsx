081fa9c9f6994ee09c97d5282121fc498675c4c2
LuzBoger <arthurbrouard2003@gmail.com>
2026-01-04 13:42:06 +0100

    feat: Add email confirmation page with i18n support.

--- infrastructure/client/src/app/[locale]/confirm/page.tsx
+++ infrastructure/client/src/app/[locale]/confirm/page.tsx
@@ -7,41 +7,44 @@ import { useContext, useEffect, useState } from "react";
 import { apiClient } from "@/lib/api/apiClient";
 import { LocaleContext } from "@/contexts/LocaleProvider";
 
+import { useTranslations } from "next-intl";
+
 
 export default function ConfirmPage() {
-    const {locale} = useContext(LocaleContext); 
-    const [message, setMessage] = useState("En attente de confirmation..");
-    const searchParams = useSearchParams();
-    const router = useRouter();
-    const pathname = usePathname();
+  const t = useTranslations('auth.confirm');
+  const { locale } = useContext(LocaleContext);
+  const [message, setMessage] = useState(t('waiting'));
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const pathname = usePathname();
 
 
-    useEffect(() => {
-         if (!pathname.startsWith(`/${locale}`)) {
+  useEffect(() => {
+    if (!pathname.startsWith(`/${locale}`)) {
       router.replace(`/${locale}/confirm?${searchParams.toString()}`);
       return;
     }
 
 
 
-        const token = searchParams.get('token');
-        if(!token) {
-            return
-        }
+    const token = searchParams.get('token');
+    if (!token) {
+      return
+    }
 
-        apiClient.get(`/auth/confirm?token=${token}`).then((res) => {
-            if(res.status === 200) {
-                setMessage("Confirmation réussie ! Vous allez être redirigé vers la page de login");
-                setTimeout(() => router.push(`/${locale}/login`), 3000)
-            }
-        });
-    }, [searchParams, router]);
+    apiClient.get(`/auth/confirm?token=${token}`).then((res) => {
+      if (res.status === 200) {
+        setMessage(t('success'));
+        setTimeout(() => router.push(`/${locale}/login`), 3000)
+      }
+    });
+  }, [searchParams, router]);
 
-    return (
-        <div className="min-h-screen flex items-center justify-center bg-gray-100 px-4">
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 px-4">
       <div className="bg-white p-8 sm:p-10 rounded-xl shadow-lg w-full max-w-md border border-gray-200 text-center">
         <h2 className="text-2xl sm:text-3xl font-semibold mb-6 text-gray-800">
-          Confirmation du compte
+          {t('title')}
         </h2>
         <p className="text-gray-600">{message}</p>
       </div>
